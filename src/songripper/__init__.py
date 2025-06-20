dc3312f7cce45397413885d36cb611903d38b3c1
rudyscoggins <rudyscoggins@gmail.com>
2025-06-20 08:13:40 -0500

    Show package update time in US Central

--- src/songripper/__init__.py
+++ src/songripper/__init__.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
 from pathlib import Path
-from datetime import datetime, timezone
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 PACKAGE_TIME = datetime.fromtimestamp(
     Path(__file__).stat().st_mtime,
-    timezone.utc,
+    tz=ZoneInfo("America/Chicago"),
 ).isoformat(timespec="seconds")
 
 __all__ = ["PACKAGE_TIME"]
