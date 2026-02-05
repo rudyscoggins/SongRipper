6a636b67bafcbfac0af39ca180e8b4e53f2181d7
RudyScoggins <rudyscoggins@gmail.com>
2026-02-05 07:00:57 -0600

    automatic cache busting

--- src/songripper/settings.py
+++ src/songripper/settings.py
@@ -1,7 +1,9 @@
 # src/songripper/settings.py
 import os
 from pathlib import Path
+from . import PACKAGE_TIME
+
 DATA_DIR = Path(os.getenv("DATA_DIR", "/data"))
 NAS_PATH  = Path(os.getenv("NAS_PATH",  "/music"))
 # Query string added to static assets for cache busting
-CACHE_BUSTER = os.getenv("CACHE_BUSTER", "20250619")
+CACHE_BUSTER = os.getenv("CACHE_BUSTER", PACKAGE_TIME.replace(":", "").replace("-", "").replace("+", ""))
